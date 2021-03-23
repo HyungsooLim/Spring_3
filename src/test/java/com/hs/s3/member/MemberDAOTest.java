c77e346350ca40bae5af4c24c7db18c4ba09a06c
HyungsooLim <hyungsoolim0@gmail.com>
2021-03-23 14:44:31 +0900

    memberJoin_progress_END
    success

--- src/test/java/com/hs/s3/member/MemberDAOTest.java
+++ src/test/java/com/hs/s3/member/MemberDAOTest.java
@@ -11,19 +11,28 @@ public class MemberDAOTest extends MyAbstractTest {
 	@Autowired
 	private MemberDAO memberDAO;
 
-	//----------- memberLogin Test --------------------------------------------------------------------------------------------
-	@Test
+	// ----------- memberLogin Test
+	// --------------------------------------------------------------------------------------------
+//	@Test
 	public void memberLoginTest() throws Exception {
 		MemberDTO memberDTO = new MemberDTO();
 		memberDTO.setId("id1");
 		memberDTO.setPw("pw1");
-		memberDTO=memberDAO.memberLogin(memberDTO);
+		memberDTO = memberDAO.memberLogin(memberDTO);
 		assertNotNull(memberDTO);
 	}
-	//----------- memberJoin Test --------------------------------------------------------------------------------------------
+
+	// ----------- memberJoin Test
+	// --------------------------------------------------------------------------------------------
 	@Test
 	public void memberJoinTest() throws Exception {
 		MemberDTO memberDTO = new MemberDTO();
-		memberDAO.memberJoin(memberDTO)
+		memberDTO.setId("id8");
+		memberDTO.setPw("pw8");
+		memberDTO.setName("name8");
+		memberDTO.setPhone("010-8888-8888");
+		memberDTO.setEmail("name8@naver.com");
+		int result = memberDAO.memberJoin(memberDTO);
+		assertEquals(1, result);
 	}
 }
