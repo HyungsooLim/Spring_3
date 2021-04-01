b158727728f6234d8e8476276fc64883f323faa3
HyungsooLim <hyungsoolim0@gmail.com>
2021-04-01 16:49:47 +0900

    js_ex_2

--- src/main/webapp/WEB-INF/views/member/memberJoin.jsp
+++ src/main/webapp/WEB-INF/views/member/memberJoin.jsp
@@ -41,16 +41,19 @@
 			<div class="form-group">
 				<label for="name">Name</label> 
 				<input type="text" class="form-control" id="name" name="name">
+				<h4 id="nameResult"></h4>
 				<!-- 비어있으면 안됨 -->
 			</div>
 			<div class="form-group">
 				<label for="phone">Phone</label> 
 				<input type="tel" class="form-control" id="phone" name="phone">
+				<h4 id="phoneResult"></h4>
 				<!-- 비어있으면 안됨 -->
 			</div>
 			<div class="form-group">
 				<label for="email">Email</label> 
 				<input type="email" class="form-control" id="email" name="email">
+				<h4 id="emailResult"></h4>
 				<!-- 비어있으면 안됨 -->
 			</div>
 			<div class="form-group form-check">
--- src/main/webapp/resources/js/memberJoin_ex1.js
+++ src/main/webapp/resources/js/memberJoin_ex1.js
@@ -34,19 +34,62 @@ pw.addEventListener("blur", function(){
 	pwResult.setAttribute("class", color);
 }); // --- 글자 수 event END ----------------------------
 
-pw2.addEventListener("change", function(){
-	
-});
+pw2.addEventListener("blur", function(){
+	if(pw.value!=pw2.value){
+		pw2.value="";
+	}
+}); // --- pw 불일치 시 pw확인 비우기 -------------------
 
-
+pw.addEventListener("change", function(){
+	pw2.value="";
+}); // --- pw 수정 시 pw확인 비우기 ---------------------
 
 
 //=== Name ====================================================================
+let name = document.getElementById("name");
+let nameResult = document.getElementById("nameResult");
 
+name.addEventListener("blur", function(){
+	let message = "";
+	let color = "";
+	if(name.value.length==0){
+		message = "필수입력사항";
+		color = "r1";
+	}
+	nameResult.innerHTML=message;
+	nameResult.setAttribute("class", color);
+}); // --- event END ---
 
 
 //=== Phone ====================================================================
+let phone = document.getElementById("phone");
+let phoneResult = document.getElementById("phoneResult");
 
-
+phone.addEventListener("blur", function(){
+	let message = "";
+	let color = "";
+	if(phone.value.length==0){
+		message="필수입력사항";
+		color = "r1";
+	}
+	phoneResult.innerHTML=message;
+	phoneResult.setAttribute("class", color);
+}); // --- event END ---
 
 //=== Email ====================================================================
+let email = document.getElementById("email");
+let emailResult = document.getElementById("emailResult");
+
+email.addEventListener("blur", function(){
+	let message = "";
+	let color = "";
+	if(email.value.length==0){
+		message="필수입력사항";
+		color = "r1";
+	}
+	emailResult.innerHTML=message;
+	emailResult.setAttribute("class", color);
+}); // --- event END ---
+
+
+//=== submit ===================================================================
