de109a5e43b49e1736ae250674431e3119dc07a6
HyungsooLim <hyungsoolim0@gmail.com>
2021-04-06 17:48:33 +0900

    jQuery_ex_2

--- src/main/webapp/resources/jquery/insertFile.js
+++ src/main/webapp/resources/jquery/insertFile.js
@@ -1,16 +1,17 @@
 /**
- * 
+ * ADD 버튼 클릭하면 files 내부에 sample 내부의 것들을 추가
+
+	한번 더 누르면 하나 더 생기게
+
+	최대 5개까지만 생성
+
+	5개 넘어가면 alert 창으로 경고
  */
+
 
 $("#add").click(function() {
 	let sample = $("#sample").html();
-	let i = 0;
-	for (let i = 0; 5; i++) {
-		if (i < 5) {
-			$("#files").append(sample);
-		}else {
-			break;
-		}
-	}
+
+	$("#files").append(sample);
 
 });
